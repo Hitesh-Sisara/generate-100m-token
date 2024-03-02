25d83f6557f585b68c1cacadfd0462f183fa57ce
Hitesh Sisara <hitesh.dev@sisara.in>
2024-03-02 21:56:56 +0530

    token

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 var jwt = require("jsonwebtoken");
 var uuid4 = require("uuid4");
 
-var app_access_key = "65e211ee1d5fd749cd1f200e";
-var app_secret =
-  "hJMdeMQ28blMIV_CqRNx6b4zWGYqB3zs4Gm6LJg7hSML-klqB5KTEzq4XJPuC113AzGakVWPgelTZXMUNjmYPog6yzD0OyImTwVKnSNu0Zc9EG3SaQTJJoNMP9De-mub8lCj9yXVinhEmD2u2IHGTKWQ3R0y1Yrd0KU9qhFqAwk=";
+var app_access_key = "<app_access_key>";
+var app_secret = "<app_secret>";
 var payload = {
   access_key: app_access_key,
   type: "management",
@@ -17,7 +16,7 @@ jwt.sign(
   app_secret,
   {
     algorithm: "HS256",
-    expiresIn: "365d",
+    expiresIn: "24h",
     jwtid: uuid4(),
   },
   function (err, token) {
